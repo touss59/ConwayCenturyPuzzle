b8b3ff2432cacb959e3ca35c962cbd75b5888f7b
Valentin Toussirot <valtoussirot@gmail.com>
2021-03-21 17:12:30 +0100

    first version of this project

---
+++ ConwayCenturyPuzzle/MainWindow.xaml.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MoreLinq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Documents;
+using System.Text;
+
+namespace ConwayCenturyPuzzle
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private Rectangle dragObject = null;
+        private Point offset;
+        private readonly List<Rectangle> rectangles;
+        private readonly DispatcherTimer dispatcherTimer;
+        private readonly char[,] position = new char[5, 4];
+        private int index = 0;
+        private List<(char shapeToMove, char direction)> response;
+
+        public object JsonConvert { get; private set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            rectangles = canvasMain.Children.OfType<UIElement>()
+                .Where(c => c.GetType().Name == nameof(Rectangle))
+                .Select(c => c as Rectangle)
+                .ToList();
+
+            var fileName = System.IO.Path.Combine(Environment.CurrentDirectory, "saveState.txt");
+
+            if (File.Exists(fileName))
+            {
+                using FileStream fs = File.OpenRead(fileName);
+                using var sr = new StreamReader(fs);
+
+                string[] line = sr.ReadLine().Split(';');
+
+                foreach (var l in line)
+                {
+                    (string name, double left, double top) = (l.Split(',')[0], Convert.ToInt32(l.Split(',')[1]), Convert.ToInt32(l.Split(',')[2]));
+                    var rectangle = rectangles.Where(r => r.Name == name).FirstOrDefault();
+                    Canvas.SetLeft(rectangle, left);
+                    Canvas.SetTop(rectangle, top);
+                }
+
+            }
+
+
+            dispatcherTimer = new DispatcherTimer
+            {
+                Interval = new TimeSpan(0, 0, 0, 0, 100)
+            };
+
+            dispatcherTimer.Tick += new EventHandler(ShowSolution);
+        }
+
+        //Methods links to solution
+        private void ShowSolution(object sender, EventArgs e)
+        {
+            if (index < response.Count)
+            {
+                var (shapeToMove, direction) = response[index];
+                Rectangle shape = rectangles.Where(r => r.Name == shapeToMove.ToString()).FirstOrDefault();
+
+                switch (direction)
+                {
+                    case 'L':
+                        Canvas.SetLeft(shape, Canvas.GetLeft(shape) - 100);
+                        break;
+                    case 'U':
+                        Canvas.SetTop(shape, Canvas.GetTop(shape) - 100);
+                        break;
+                    case 'R':
+                        Canvas.SetLeft(shape, Canvas.GetLeft(shape) + 100);
+                        break;
+                    case 'D':
+                        Canvas.SetTop(shape, Canvas.GetTop(shape) + 100);
+                        break;
+                    default:
+                        Console.WriteLine("Default case");
+                        break;
+                }
+                index++;
+            }
+            else
+            {
+                dispatcherTimer.Stop();
+            }
+        }
+
+        public void Window_Closing(object sender, CancelEventArgs e)
+        {
+            string rtfFile = System.IO.Path.Combine(Environment.CurrentDirectory, "saveState.txt");
+            FileStream fileStream = File.Create(rtfFile);
+            using var sr = new StreamWriter(fileStream);
+            var state = string.Join(';', rectangles.Where(r => !string.IsNullOrEmpty(r.Name) && r.Name != "Exit").Select(r => r.Name + ',' + Math.Round(Canvas.GetLeft(r)).ToString() + ',' + Math.Round(Canvas.GetTop(r)).ToString()));
+            sr.Write(state);
+
+        }
+
+        public void ExecuteSolution(object sender, KeyEventArgs e)
+        {
+            if ((e.Key == Key.Enter))
+            {
+                for (var y = 0; y < position.GetLength(0); y++)
+                {
+                    for (var x = 0; x < position.GetLength(1); x++)
+                    {
+                        position[y, x] = 'A';
+                    }
+                }
+                index = 0;
+                GetPositionRectangles();
+                response = Solver.ShowMove(position);
+                dispatcherTimer.Start();
+            } else if (e.Key == Key.R)
+            {
+                dispatcherTimer.Stop();
+                ResetPositions();
+            }
+        }
+
+        private void GetPositionRectangles()
+        {
+
+            foreach (var rectangle in rectangles)
+            {
+                Rect rect = new Rect(Canvas.GetLeft(rectangle), Canvas.GetTop(rectangle), rectangle.Width, rectangle.Height);
+                GetPositionRectangle(rect, rectangle.Name);
+            }
+        }
+
+        private void GetPositionRectangle(Rect rect, string Name)
+        {
+            int minX = Convert.ToInt32(rect.TopLeft.X / 100) - 1;
+            int minY = Convert.ToInt32(rect.TopLeft.Y / 100) - 1;
+
+            int maxX = Convert.ToInt32(rect.BottomRight.X / 100) - 2;
+            int maxY = Convert.ToInt32(rect.BottomRight.Y / 100) - 2;
+
+            for (var y = minY; y <= maxY; y++)
+            {
+                for (var x = minX; x <= maxX; x++)
+                {
+                    position[y, x] = Name[0];
+                }
+            }
+        }
+        /*********************************************************************/
+
+
+
+        private void UserControlPreviewMouseDown(object sender, MouseEventArgs e)
+        {
+            this.dragObject = sender as Rectangle;
+            this.offset = e.GetPosition(this.canvasMain);
+            this.offset.Y -= Canvas.GetTop(this.dragObject);
+            this.offset.X -= Canvas.GetLeft(this.dragObject);
+            this.canvasMain.CaptureMouse();
+
+        }
+
+        private void CanvasMainMouseUp(object sender, MouseEventArgs e)
+        {
+            this.dragObject = null;
+            this.canvasMain.ReleaseMouseCapture();
+        }
+
+
+
+
+        //Methods links with drag and drop shapes
+        private void CanvasMainMouseMove(object sender, MouseEventArgs e)
+        {
+            if (this.dragObject == null)
+            {
+                return;
+            }
+
+            var newPosition = e.GetPosition(sender as IInputElement);
+
+            if (!checkMouseOnDragObject(newPosition)) {
+                this.dragObject = null;
+                return;
+            }
+
+
+
+            (bool colX, bool colY) = CheckCollisionWithOtherRectangles(newPosition, this.dragObject);
+
+            if (!colX)
+            {
+                Canvas.SetLeft(this.dragObject, newPosition.X - this.offset.X);
+            }
+
+            if (!colY)
+            {
+                Canvas.SetTop(this.dragObject, newPosition.Y - this.offset.Y);
+            }
+        }
+
+        private bool checkMouseOnDragObject(Point mouse)
+        {
+            Rect dragO = new Rect(Canvas.GetLeft(dragObject), Canvas.GetTop(dragObject), dragObject.Width, dragObject.Height);
+            Rect m = new Rect(mouse.X, mouse.Y, 10, 10);
+
+            return dragO.IntersectsWith(m);
+        }
+
+        private (bool colX, bool colY) CheckCollisionWithOtherRectangles(Point newPosition, Rectangle dragObject)
+        {
+            bool collisionY = false;
+            bool collisionX = false;
+            foreach (var r in rectangles)
+            {
+                if (r != dragObject && !(r.Name == "Exit" && dragObject.Name == "J"))
+                {
+                    Rect thisRectX = new Rect(newPosition.X - this.offset.X + 6, Canvas.GetTop(dragObject) + 6, dragObject.Width - 12, dragObject.Height - 12);
+                    Rect thisRectY = new Rect(Canvas.GetLeft(dragObject) + 6, newPosition.Y - this.offset.Y + 6, dragObject.Width - 12, dragObject.Height - 12);
+                    Rect otherRect = new Rect(Canvas.GetLeft(r), Canvas.GetTop(r), r.Width, r.Height);
+
+                    collisionX = collisionX == true ? collisionX : thisRectX.IntersectsWith(otherRect);
+
+                    collisionY = collisionY == true ? collisionY : thisRectY.IntersectsWith(otherRect);
+
+                }
+
+            }
+
+            return (collisionX, collisionY);
+
+        }
+
+        private void ResetPositions()
+        {
+            Canvas.SetLeft(V, 100);
+            Canvas.SetTop(V, 200);
+
+            Canvas.SetLeft(X, 200);
+            Canvas.SetTop(X, 300);
+
+            Canvas.SetLeft(W, 400);
+            Canvas.SetTop(W, 200);
+
+            Canvas.SetLeft(R, 100);
+            Canvas.SetTop(R, 100);
+
+            Canvas.SetLeft(r, 400);
+            Canvas.SetTop(r, 100);
+
+            Canvas.SetLeft(k, 100);
+            Canvas.SetTop(k, 400);
+
+            Canvas.SetLeft(K, 400);
+            Canvas.SetTop(K, 400);
+
+            Canvas.SetLeft(M, 300);
+            Canvas.SetTop(M, 500);
+
+            Canvas.SetLeft(Y, 100);
+            Canvas.SetTop(Y, 500);
+
+            Canvas.SetLeft(J, 200);
+            Canvas.SetTop(J, 100);
+
+        }
+
+
+    }
+}
---
+++ ConwayCenturyPuzzle/Solver.cs
@@ -0,0 +1,401 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConwayCenturyPuzzle
+{
+    public class Solver
+    {
+        public static List<char[,]> Solve(char[,] grid)
+        {
+            var firstPosition = StockGrid(grid);
+            (string historique, string positionNow) positionNow = (firstPosition, firstPosition);
+
+            Queue<(string historique, string positionNow)> nodesToExplore = new Queue<(string historique, string positionNow)> { };
+            Dictionary<string, string> nodesExplored = new Dictionary<string, string> { };
+            HashSet<string> positionVisited = new HashSet<string> { };
+            nodesToExplore.Enqueue(positionNow);
+            bool isSolve = false;
+
+            nodesExplored.Add(firstPosition, "start");
+            positionVisited.Add(SavePosition(firstPosition));
+
+            while (nodesToExplore.Count > 0 && !isSolve)
+            {
+                positionNow = nodesToExplore.Dequeue();
+
+                grid = GetGrid(positionNow.positionNow);
+
+                List<char> pieceVisited = new List<char> { };
+                List<string> newPositions = new List<string> { };
+
+                for (var y = 0; y < grid.GetLength(0); y++)
+                {
+                    for (var x = 0; x < grid.GetLength(1); x++)
+                    {
+                        if (!pieceVisited.Contains(grid[y, x]) && grid[y, x] != 'A')
+                        {
+                            pieceVisited.Add(grid[y, x]);
+                            var dimP = GetDimPiece(grid[y, x], grid);
+                            var (left, right, up, down) = WhereItCanMove(dimP, grid);
+
+                            if (left)
+                            {
+                                newPositions.Add(Move(dimP, grid, 'L'));
+                            }
+                            if (right)
+                            {
+                                newPositions.Add(Move(dimP, grid, 'R'));
+                            }
+                            if (up)
+                            {
+                                newPositions.Add(Move(dimP, grid, 'U'));
+                            }
+                            if (down)
+                            {
+                                newPositions.Add(Move(dimP, grid, 'D'));
+                            }
+                        }
+                    }
+                }
+                foreach (var newPos in newPositions)
+                {
+                    if (!positionVisited.Contains(SavePosition(newPos)) && !positionVisited.Contains(GetSymmetry(SavePosition(newPos))))
+                    {
+                        (string historique, string positionNow) position = (positionNow.positionNow, newPos);
+                        nodesExplored.Add(newPos, positionNow.positionNow);
+                        positionVisited.Add(SavePosition(newPos));
+                        nodesToExplore.Enqueue(position);
+
+                        if (newPos[13] == 'J' && newPos[14] == 'J' && newPos[17] == 'J' && newPos[18] == 'J')
+                        {
+                            nodesExplored.Add("win", newPos);
+                            isSolve = true;
+                            break;
+                        }
+
+
+                    }
+                }
+            }
+
+            string key = "win";
+
+            List<char[,]> response = new List<char[,]> { };
+
+            while (1 == 1)
+            {
+                key = nodesExplored[key];
+
+                char[,] step = new char[5, 4];
+
+                if (key == "start")
+                {
+                    break;
+                }
+
+                var index = 0;
+                for (var y = 0; y < grid.GetLength(0); y++)
+                {
+                    for (var x = 0; x < grid.GetLength(1); x++)
+                    {
+                        step[y, x] = key[index];
+                        index++;
+                    }
+                }
+
+                response.Add(step);
+            }
+            return response;
+        }
+
+        public static (int xMin, int xMax, int yMin, int yMax) GetDimPiece(char piece, char[,] grid)
+        {
+            (int xMin, int xMax, int yMin, int yMax) = (999, 0, 999, 0);
+            for (var y = 0; y < grid.GetLength(0); y++)
+            {
+                for (var x = 0; x < grid.GetLength(1); x++)
+                {
+                    if (grid[y, x] == piece)
+                    {
+                        xMin = xMin <= x ? xMin : x;
+                        xMax = xMax >= x ? xMax : x;
+                        yMin = yMin <= y ? yMin : y;
+                        yMax = yMax >= y ? yMax : y;
+                    }
+                }
+            }
+
+            return (xMin, xMax, yMin, yMax);
+        }
+
+        public static (bool left, bool right, bool up, bool down) WhereItCanMove((int xMin, int xMax, int yMin, int yMax) dimension, char[,] grid)
+        {
+            (bool left, bool right, bool up, bool down) = (true, true, true, true);
+
+            //check move left
+            if (dimension.xMin - 1 >= 0)
+            {
+                for (var y = dimension.yMin; y <= dimension.yMax; y++)
+                {
+                    if (grid[y, dimension.xMin - 1] != 'A')
+                    {
+                        left = false;
+                    }
+                }
+            }
+            else
+            {
+                left = false;
+            }
+
+            //check move right
+            if (dimension.xMax + 1 < grid.GetLength(1))
+            {
+                for (var y = dimension.yMin; y <= dimension.yMax; y++)
+                {
+                    if (grid[y, dimension.xMax + 1] != 'A')
+                    {
+                        right = false;
+                    }
+                }
+            }
+            else
+            {
+                right = false;
+            }
+
+            //check move up
+            if (dimension.yMin - 1 >= 0)
+            {
+                for (var x = dimension.xMin; x <= dimension.xMax; x++)
+                {
+                    if (grid[dimension.yMin - 1, x] != 'A')
+                    {
+                        up = false;
+                    }
+                }
+            }
+            else
+            {
+                up = false;
+            }
+
+            //check move down
+            if (dimension.yMax + 1 < grid.GetLength(0))
+            {
+                for (var x = dimension.xMin; x <= dimension.xMax; x++)
+                {
+                    if (grid[dimension.yMax + 1, x] != 'A')
+                    {
+                        down = false;
+                    }
+                }
+            }
+            else
+            {
+                down = false;
+            }
+
+            return (left, right, up, down);
+        }
+
+        public static string Move((int xMin, int xMax, int yMin, int yMax) dimension, char[,] grid, char direction)
+        {
+            char[,] gridMoved = CopieGrid(grid);
+            if (direction == 'U')
+            {
+                for (var x = dimension.xMin; x <= dimension.xMax; x++)
+                {
+                    gridMoved[dimension.yMin - 1, x] = grid[dimension.yMin, x];
+                    gridMoved[dimension.yMax, x] = 'A';
+                }
+                return StockGrid(gridMoved);
+            }
+
+            if (direction == 'D')
+            {
+                for (var x = dimension.xMin; x <= dimension.xMax; x++)
+                {
+                    gridMoved[dimension.yMin, x] = 'A';
+                    gridMoved[dimension.yMax + 1, x] = grid[dimension.yMax, x];
+                }
+                return StockGrid(gridMoved);
+            }
+
+            if (direction == 'L')
+            {
+                for (var y = dimension.yMin; y <= dimension.yMax; y++)
+                {
+                    gridMoved[y, dimension.xMin - 1] = grid[y, dimension.xMin];
+                    gridMoved[y, dimension.xMax] = 'A';
+                }
+                return StockGrid(gridMoved);
+            }
+
+            if (direction == 'R')
+            {
+                for (var y = dimension.yMin; y <= dimension.yMax; y++)
+                {
+                    gridMoved[y, dimension.xMax + 1] = grid[y, dimension.xMax];
+                    gridMoved[y, dimension.xMin] = 'A';
+                }
+                return StockGrid(gridMoved);
+            }
+
+            return StockGrid(gridMoved);
+        }
+
+        public static char[,] CopieGrid(char[,] grid)
+        {
+            char[,] newGrid = new char[grid.GetLength(0), grid.GetLength(1)];
+
+            for (var y = 0; y < grid.GetLength(0); y++)
+            {
+                for (var x = 0; x < grid.GetLength(1); x++)
+                {
+                    newGrid[y, x] = grid[y, x];
+                }
+            }
+            return newGrid;
+        }
+
+        public static string StockGrid(char[,] grid)
+        {
+            string result = "";
+
+            foreach (var piece in grid)
+            {
+                result += piece;
+            }
+            return result;
+        }
+
+        public static char[,] GetGrid(string gridStocked)
+        {
+            char[,] grid = new char[5, 4];
+            int i = 0;
+
+            for (var y = 0; y < grid.GetLength(0); y++)
+            {
+                for (var x = 0; x < grid.GetLength(1); x++)
+                {
+                    grid[y, x] = gridStocked[i];
+                    i++;
+                }
+            }
+            return grid;
+        }
+
+        public static string GetSymmetry(string pos)
+        {
+            char[,] symmetry = GetGrid(pos);
+
+            for (var y = 0; y < symmetry.GetLength(0); y++)
+            {
+                List<char> pieces = new List<char> { };
+                for (var x = 0; x < symmetry.GetLength(1); x++)
+                {
+                    pieces.Add(symmetry[y, x]);
+                }
+
+                symmetry[y, 0] = pieces[3];
+                symmetry[y, 1] = pieces[2];
+                symmetry[y, 2] = pieces[0];
+                symmetry[y, 3] = pieces[1];
+            }
+
+            return StockGrid(symmetry);
+        }
+
+        public static string SavePosition(string pos)
+        {
+            string savePos = "";
+            for (var i = 0; i < pos.Length; i++)
+            {
+                if (pos[i] == 'X' || pos[i] == 'W')
+                {
+                    savePos += "V";
+                }
+                else if (pos[i] == 'r' || pos[i] == 'K' || pos[i] == 'k')
+                {
+                    savePos += 'R';
+                }
+                else if (pos[i] == 'Y')
+                {
+                    savePos += "M";
+                }
+                else
+                {
+                    savePos += pos[i];
+                }
+            }
+            return savePos;
+        }
+
+
+        public static List<(char shapeToMove, char direction)> ShowMove(char[,] grid)
+        {
+            List<char[,]> response = Solve(grid);
+            response.Reverse();
+            List<(char,char)> moves = new List<(char,char)> { };
+
+
+            for (var i = 1; i < response.Count; i++)
+            {
+                moves.Add(GetMove(response[i - 1], response[i]));
+            }
+            return moves;
+        }
+
+
+        public static (char shapeMove, char direction) GetMove(char[,] previous, char[,] now)
+        {
+            char shapeMove = GetShape(previous, now);
+
+            (int xMin, int xMax, int yMin, int yMax) dimP = GetDimPiece(shapeMove, previous);
+            (int xMin, int xMax, int yMin, int yMax) dimN = GetDimPiece(shapeMove, now);
+
+            char direction = GetDirection(dimP, dimN);
+
+            return (shapeMove, direction);
+
+        }
+
+        public static char GetShape(char[,] previous, char[,] now)
+        {
+            for (var y = 0; y < previous.GetLength(0); y++)
+            {
+                for (var x = 0; x < previous.GetLength(1); x++)
+                {
+                    if (previous[y, x] != now[y, x] && now[y, x] != 'A')
+                    {
+                        return now[y, x];
+                    }
+                }
+            }
+            return 'T';
+        }
+
+        public static char GetDirection((int xMin, int xMax, int yMin, int yMax) dimP, (int xMin, int xMax, int yMin, int yMax) dimN)
+        {
+            if (dimN.xMin < dimP.xMin)
+            {
+                return 'L';
+            }
+            if (dimN.xMin > dimP.xMin)
+            {
+                return 'R';
+            }
+
+            if (dimN.yMin < dimP.yMin)
+            {
+                return 'U';
+            }
+            else
+            {
+                return 'D';
+            }
+        }
+    }
+}
